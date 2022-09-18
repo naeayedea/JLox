32aba901671fb3e851a0aa3912eaba5b663fa9cf
naeayedea <steven.muirhead1999@gmail.com>
2022-09-18 22:25:23 +0100

    function closure works

--- src/io/github/naeayedea/lox/LoxFunction.java
+++ src/io/github/naeayedea/lox/LoxFunction.java
@@ -5,15 +5,17 @@ import java.util.List;
 public class LoxFunction implements LoxCallable {
 
     private final Stmt.Function declaration;
+    private final Environment closure;
 
-    public LoxFunction(Stmt.Function declaration) {
+    public LoxFunction(Stmt.Function declaration, Environment closure) {
         this.declaration = declaration;
+        this.closure = closure;
     }
 
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
         //create a new environment for the function, e.g. function should only be able to see and access global variables, functions
-        Environment environment = new Environment(interpreter.globals);
+        Environment environment = new Environment(closure);
         //input the functions parameters into its environment
         for (int i = 0; i < declaration.params.size(); i++) {
             environment.define(declaration.params.get(i).lexeme, arguments.get(i));
